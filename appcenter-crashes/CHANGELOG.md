8695a87370f476ff3be8f64851c516230066f70d
John Borges <jaborges@protonmail.ch>
2021-06-24 19:47:39 -0400

    docs(crashes): cleanup jsdocs

--- appcenter-crashes/CHANGELOG.md
+++ appcenter-crashes/CHANGELOG.md
@@ -1,5 +1,11 @@
 # Change Log
 
+## 0.3.1
+
+* Update `Appcenter/Analytics` to 4.2.0
+* Update `AppCenterCapacitorShared` to 0.3.1
+* Update `docgen` to 0.0.17
+
 ## 0.3.0
 
 ### Features
--- appcenter-crashes/src/definitions.ts
+++ appcenter-crashes/src/definitions.ts
@@ -5,57 +5,105 @@ export enum UserConfirmation {
 }
 
 export interface Device {
-  /* Name of the SDK. Consists of the name of the SDK and the platform, e.g. "appcenter.ios", "appcenter.android" */
+  /** 
+   * Name of the SDK. Consists of the name of the SDK and the platform, e.g. "appcenter.ios", "appcenter.android" 
+   */
   sdkName: string;
-  /* Version of the SDK in semver format, e.g. "1.2.0" or "0.12.3-alpha.1". */
+  /**
+   * Version of the SDK in semver format, e.g. "1.2.0" or "0.12.3-alpha.1". 
+   */
   sdkVersion: string;
-  /* Device model (example: iPad2,3). */
+  /**
+   * Device model (example: iPad2,3). 
+   */
   model: string;
-  /* Device manufacturer (example: HTC). */
+  /**
+   * Device manufacturer (example: HTC). 
+   */
   oemName: string;
-  /* OS name (example: iOS). */
+  /**
+   * OS name (example: iOS). 
+   */
   osName: string;
-  /* OS version (example: 9.3.0). */
+  /**
+   * OS version (example: 9.3.0). 
+   */
   osVersion: string;
-  /* OS build code (example: LMY47X). */
+  /**
+   * OS build code (example: LMY47X). 
+   */
   osBuild?: string;
-  /* API level when applicable like in Android (example: 15). */
+  /**
+   * API level when applicable like in Android (example: 15). 
+   */
   osApiLevel?: number;
-  /* Language code (example: en_US). */
+  /**
+   * Language code (example: en_US). 
+   */
   locale: string;
-  /* The offset in minutes from UTC for the device time zone, including daylight savings time. */
+  /**
+   * The offset in minutes from UTC for the device time zone, including daylight savings time. 
+   */
   timeZoneOffset: number;
-  /* Screen size of the device in pixels (example: 640x480). */
+  /**
+   * Screen size of the device in pixels (example: 640x480). 
+   */
   screenSize: string;
-  /* Application version name, e.g. 1.1.0 */
+  /**
+   * Application version name, e.g. 1.1.0 
+   */
   appVersion: string;
-  /* Carrier name (for mobile devices). */
+  /**
+   * Carrier name (for mobile devices). 
+   */
   carrierName?: string;
-  /* Carrier country code (for mobile devices). */
+  /**
+   * Carrier country code (for mobile devices). 
+   */
   carrierCountry?: string;
-  /* The app's build number, e.g. 42. */
+  /**
+   * The app's build number, e.g. 42. 
+   */
   appBuild: string;
-  /* The bundle identifier, package identifier, or namespace, depending on what the individual plattforms use,  .e.g com.microsoft.example. */
+  /**
+   * The bundle identifier, package identifier, or namespace, depending on what the individual plattforms use,  .e.g com.microsoft.example. 
+   */
   appNamespace?: string;
 }
 
 export interface ErrorReport {
-  /* UUID for the crash report. */
+  /**
+   * UUID for the crash report. 
+   */
   id: string;
   threadName?: string;
-  /* Date and time the error occurred. */
+  /**
+   * Date and time the error occurred. 
+   */
   appErrorTime?: string | number;
-  /* Date and time the app started. */
+  /**
+   * Date and time the app started. 
+   */
   appStartTime?: string | number;
-  /* Exception name that triggered the crash. */
+  /**
+   * Exception name that triggered the crash. 
+   */
   exceptionName?: string;
-  /* Exception reason. */
+  /**
+   * Exception reason. 
+   */
   exceptionReason?: string;
-  /* Device information of the app when it crashed. */
+  /**
+   * Device information of the app when it crashed. 
+   */
   device: Device;
-  /* Signal that caused the crash. */
+  /**
+   * Signal that caused the crash. 
+   */
   signal: string;
-  /* Identifier of the app process that crashed. */
+  /**
+   * Identifier of the app process that crashed. 
+   */
   appProcessIdentifier: number;
 }
 
@@ -69,11 +117,15 @@ export interface CrashesListener {
 }
 
 export class ErrorAttachmentLog {
-  // Create text attachment for an error report
+  /**
+   * Create text attachment for an error report
+   */
   public static attachmentWithText(text: string, fileName?: string): ErrorAttachmentLog {
     return { text, fileName };
   }
-  // Create binary attachment for an error report, binary must be passed as a base64 string
+  /**
+   * Create binary attachment for an error report, binary must be passed as a base64 string
+   */
   public static attachmentWithBinary(
     data: string,
     fileName: string | null,
@@ -137,7 +189,7 @@ export interface CrashesPlugin {
     * 
     * const { value: hasCrashed } = await Crashes.hasCrashedInLastSession();
     */
-    hasCrashedInLastSession(): Promise<{ value: boolean}>;
+    hasCrashedInLastSession(): Promise<{value: boolean}>;
 
     /**
      * Provides details about the crash that occurred in the last app session.
