9ff19ea8956e880d06525b12df41f0be2c3f83aa
John Borges <jaborges@protonmail.ch>
2021-09-14 10:51:16 -0400

    feat(analytics): android support

---
+++ appcenter-analytics/android/src/main/java/com/getcapacitor/plugin/appcenter/analytics/AnalyticsBase.java
@@ -0,0 +1,52 @@
+package com.getcapacitor.plugin.appcenter.analytics;
+
+import java.util.Map;
+
+import com.microsoft.appcenter.AppCenter;
+import com.microsoft.appcenter.analytics.Analytics;
+import com.microsoft.appcenter.Flags;
+
+public class AnalyticsBase {
+
+    public void trackEvent(String name, Map<String, String> properties, String flag) {
+        int analyticsFlag;
+
+        switch (flag) {
+            case "critical":
+                analyticsFlag = Flags.CRITICAL;
+                break;
+            case "normal":
+                analyticsFlag = Flags.NORMAL;
+                break;
+            default:
+                analyticsFlag = Flags.NORMAL;
+                break;
+        }
+        Analytics.trackEvent(name, properties, analyticsFlag);
+    }
+
+    public void setTransmissionInterval(int seconds) {
+        Analytics.setTransmissionInterval(seconds);
+    }
+
+    public void start() {
+        AppCenter.start(Analytics.class);
+    }
+
+    public void pause() {
+        Analytics.pause();
+    }
+
+    public void resume() {
+        Analytics.resume();
+    }
+
+    public void enable(boolean enable) {
+        Analytics.setEnabled(enable).get();
+    }
+
+    public boolean isEnabled() {
+        return Analytics.isEnabled().get();
+    }
+
+}
