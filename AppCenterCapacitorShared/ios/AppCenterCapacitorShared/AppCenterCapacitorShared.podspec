fe0451fd8547fdc7c169e83fb649a768e05745a7
John Borges <jaborges@protonmail.ch>
2021-10-01 10:24:16 -0400

    style(shared): lint and podspec fixes

--- AppCenterCapacitorShared/ios/AppCenterCapacitorShared/AppCenterCapacitorShared.podspec
+++ AppCenterCapacitorShared/ios/AppCenterCapacitorShared/AppCenterCapacitorShared.podspec
@@ -1,15 +1,16 @@
 Pod::Spec.new do |s|
   s.name             = 'AppCenterCapacitorShared'
-  s.version          = '0.1.0'
+  s.version          = '0.3.3'
   s.summary          = 'Shared utility to assist with bootstrapping AppCenter for Capacitor plugin.'
   s.homepage         = 'https://github.com/capacitor-community/appcenter-sdk-capacitor'
   s.license          = { :type => 'MIT', :file => 'LICENSE' }
   s.author           = { 'johnborges' => 'john.borges@icloud.com' }
-  s.source           = { :http => 'https://github.com/capacitor-community/appcenter-sdk-capacitor/releases/download/0.1.0/AppCenter-SDK-Capacitor-iOS-Pod-0.1.0.zip' }
+  s.source           = { :git => 'https://github.com/capacitor-community/appcenter-sdk-capacitor.git' , :tag => s.version.to_s }
   s.social_media_url = 'https://twitter.com/johnborges'
   s.ios.deployment_target = '12.0'
   s.swift_version = '5.1'
-  s.source_files = '*.swift'
-  s.dependency 'AppCenter/Core', '4.1.1'
+  # s.source_files = '*.swift'
+  s.dependency 'AppCenter/Core', '4.3.0'
   s.static_framework = true
+  # s.vendored_frameworks = '**/AppCenterCapacitorShared.xcframework'
 end
--- AppCenterCapacitorShared/ios/AppCenterCapacitorShared/AppCenterCapacitorShared.swift
+++ AppCenterCapacitorShared/ios/AppCenterCapacitorShared/AppCenterCapacitorShared.swift
@@ -2,66 +2,86 @@ import Foundation
 import AppCenter
 
 public class AppCenterCapacitorShared: NSObject {
-    
+
     static var startAutomatically: Bool = true
     static var configuration: NSDictionary = [:]
     static var appSecret: String?
+    static var logLevel: UInt?
     static var wrapperSdk: WrapperSdk?
     // plist keys
     static var kAppCenterSecretKey = "AppSecret"
     static var kAppCenterStartAutomaticallyKey = "StartAutomatically"
+    static var kAppCenterLogLevelKey = "LogLevel"
     static var kAppCenterConfigResource = "AppCenter-Config"
-    
+
+    public static func isSdkConfigured() -> Bool {
+        return AppCenter.isConfigured
+    }
+
     public static func getConfiguration() -> NSDictionary {
         return configuration
     }
-    
+
     public static func setStartAutomatically (_ shouldStartAutomatically: Bool) {
-        startAutomatically = shouldStartAutomatically;
+        startAutomatically = shouldStartAutomatically
     }
-    
+
     public static func configureWithSettings() {
         if AppCenter.isConfigured {
             return
         }
-        
-        let wrapperSdk = WrapperSdk(wrapperSdkVersion: "0.1.0", wrapperSdkName: "appcenter.capacitor", wrapperRuntimeVersion: nil, liveUpdateReleaseLabel: nil, liveUpdateDeploymentKey: nil, liveUpdatePackageHash: nil)
+
+        let wrapperSdk = WrapperSdk(wrapperSdkVersion: "0.3.2",
+            wrapperSdkName: "appcenter.capacitor",
+            wrapperRuntimeVersion: nil,
+            liveUpdateReleaseLabel: nil,
+            liveUpdateDeploymentKey: nil,
+            liveUpdatePackageHash: nil)
 
         setWrapperSdk(wrapperSdk!)
-        getAppSecret()
-                
+        getSdkConfig()
+
         if startAutomatically {
             if appSecret!.count == 0 {
                 AppCenter.configure()
-            }
-            else {
+            } else {
                 AppCenter.configure(withAppSecret: appSecret)
+            }
+
+            // set log level if specified
+            if logLevel != nil {
+                AppCenter.logLevel = LogLevel.init(rawValue: logLevel!) ?? .assert
             }
         }
     }
-    
+
     public static func setAppSecret(_ secret: String) {
         appSecret = secret
     }
-    
-    public static func getAppSecret() {
+
+    public static func getSdkConfig() {
         if appSecret == nil {
-            // get values from config plist
+            // get config plist
             let plist = Bundle.main.path(forResource: AppCenterCapacitorShared.kAppCenterConfigResource, ofType: "plist")
             configuration = NSDictionary(contentsOfFile: plist!)!
+
+            // get appSecret key
             appSecret = configuration[AppCenterCapacitorShared.kAppCenterSecretKey] as? String
-            
+
+            // get log level
+            logLevel = configuration[AppCenterCapacitorShared.kAppCenterLogLevelKey] as? UInt
+
             // start automatically flag true by default
             if let rawStartAutomatically = configuration[AppCenterCapacitorShared.kAppCenterStartAutomaticallyKey] as? Bool {
                 startAutomatically = rawStartAutomatically
             }
         }
     }
-    
+
     public static func getWrapperSdk() -> WrapperSdk? {
         return wrapperSdk
     }
-    
+
     public static func setWrapperSdk(_ sdk: WrapperSdk) {
         wrapperSdk = sdk
         AppCenter.wrapperSdk = sdk
